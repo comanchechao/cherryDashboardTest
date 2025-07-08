27e704425f84a3f4050c119d95a7ba1bf67740e4
comanchechao <chao.comanche@gmail.com>
2025-07-09 00:11:58 +0330

    UPdate

--- src/pages/cherrySniper.tsx
+++ src/pages/cherrySniper.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Icon } from "@iconify/react";
 import Navbar from "../layouts/Navbar";
