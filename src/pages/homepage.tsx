beebcf03a18b35b58133f9ea8f87b3a8060feaa8
comanchechao <chao.comanche@gmail.com>
2025-08-20 14:39:40 +0330

    Update homepage.tsx

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -123,7 +123,6 @@ const HomePage: React.FC = () => {
 
   const [heroAnimated, setHeroAnimated] = useState(false);
 
-  const featureCardsRef = useRef<HTMLDivElement>(null);
   const featureCard1Ref = useRef<HTMLDivElement>(null);
   const featureCard2Ref = useRef<HTMLDivElement>(null);
   const featureCard3Ref = useRef<HTMLDivElement>(null);
@@ -142,7 +141,7 @@ const HomePage: React.FC = () => {
   // const [alreadySubscribedToastVisible, setAlreadySubscribedToastVisible] =
   //   useState(false);
 
-  const [activeTab, setActiveTab] = useState<"manual" | "autobot">("manual");
+  const [activeTab] = useState<"manual" | "autobot">("manual");
 
   const [activePartner, setActivePartner] = useState<number>(0);
 
@@ -657,44 +656,6 @@ const HomePage: React.FC = () => {
                   </div>
 
                   <div className="hero-buttons w-full flex flex-col sm:flex-row items-center justify-center gap-6 mb-9">
-                    <button
-                      onClick={() => {
-                        window.open("https://docs.cherrybot.co/", "_blank");
-                      }}
-                      className="btn-wave-secondary cursor-pointer w-full"
-                    >
-                      <span className="wave-bg"></span>
-                      <span className="wave-left">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="w-auto h-full opacity-100 object-stretch"
-                          viewBox="0 0 487 487"
-                        >
-                          <path
-                            fillOpacity=".1"
-                            fillRule="nonzero"
-                            fill="currentColor"
-                            d="M0 .3c67 2.1 134.1 4.3 186.3 37 52.2 32.7 89.6 95.8 112.8 150.6 23.2 54.8 32.3 101.4 61.2 149.9 28.9 48.4 77.7 98.8 126.4 149.2H0V.3z"
-                          ></path>
-                        </svg>
-                      </span>
-                      <span className="wave-right">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="object-cover w-full h-full"
-                          viewBox="0 0 487 487"
-                        >
-                          <path
-                            fillOpacity=".1"
-                            fillRule="nonzero"
-                            fill="currentColor"
-                            d="M487 486.7c-66.1-3.6-132.3-7.3-186.3-37s-95.9-85.3-126.2-137.2c-30.4-51.8-49.3-99.9-76.5-151.4C70.9 109.6 35.6 54.8.3 0H487v486.7z"
-                          ></path>
-                        </svg>
-                      </span>
-                      <span className="wave-overlay"></span>
-                      <span className="btn-text">Learn More</span>
-                    </button>{" "}
                     <button
                       onClick={() => {
                         window.open("/dashboard");
