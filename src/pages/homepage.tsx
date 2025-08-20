811288b1ddcbce326000dca4a709550f82c6a23f
comanchechao <chao.comanche@gmail.com>
2025-08-20 21:22:44 +0330

    Update homepage.tsx

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -160,6 +160,10 @@ const HomePage: React.FC = () => {
     window.open("https://t.me/CherryTGBot", "_blank");
   };
 
+  const handleExploreAnalytics = () => {
+    window.open("https://t.me/cherrytrending", "_blank");
+  };
+
   useEffect(() => {
     const styleSheet = document.createElement("style");
     styleSheet.type = "text/css";
@@ -1069,6 +1073,114 @@ const HomePage: React.FC = () => {
                                   </p>
                                 </div>
                               </div>
+                            </div>
+                            <div className="flex w-full items-center justify-center gap-4 ">
+                              <button
+                                onClick={handleExploreAnalytics}
+                                className="btn-wave-primary cursor-pointer flex items-center mt-4 gap-2 text-xl"
+                              >
+                                <span className="wave-bg"></span>
+                                <span className="wave-left">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-auto h-full opacity-100 object-stretch"
+                                    viewBox="0 0 487 487"
+                                  >
+                                    <path
+                                      fillOpacity=".1"
+                                      fillRule="nonzero"
+                                      fill="#FFF"
+                                      d="M0 .3c67 2.1 134.1 4.3 186.3 37 52.2 32.7 89.6 95.8 112.8 150.6 23.2 54.8 32.3 101.4 61.2 149.9 28.9 48.4 77.7 98.8 126.4 149.2H0V.3z"
+                                    ></path>
+                                  </svg>
+                                </span>
+                                <span className="wave-right">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="object-cover w-full h-full"
+                                    viewBox="0 0 487 487"
+                                  >
+                                    <path
+                                      fillOpacity=".1"
+                                      fillRule="nonzero"
+                                      fill="#FFF"
+                                      d="M487 486.7c-66.1-3.6-132.3-7.3-186.3-37s-95.9-85.3-126.2-137.2c-30.4-51.8-49.3-99.9-76.5-151.4C70.9 109.6 35.6 54.8.3 0H487v486.7z"
+                                    ></path>
+                                  </svg>
+                                </span>
+                                <span className="wave-overlay"></span>
+                                <span className="btn-text">
+                                  Explore AI Analytics
+                                </span>
+                                <svg
+                                  width="24"
+                                  height="24"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path
+                                    d="M5 12H19M19 12L12 5M19 12L12 19"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                  />
+                                </svg>
+                              </button>{" "}
+                              <button
+                                onClick={handleExploreBot}
+                                className="btn-wave-primary cursor-pointer flex items-center mt-4 gap-2 text-xl"
+                              >
+                                <span className="wave-bg"></span>
+                                <span className="wave-left">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-auto h-full opacity-100 object-stretch"
+                                    viewBox="0 0 487 487"
+                                  >
+                                    <path
+                                      fillOpacity=".1"
+                                      fillRule="nonzero"
+                                      fill="#FFF"
+                                      d="M0 .3c67 2.1 134.1 4.3 186.3 37 52.2 32.7 89.6 95.8 112.8 150.6 23.2 54.8 32.3 101.4 61.2 149.9 28.9 48.4 77.7 98.8 126.4 149.2H0V.3z"
+                                    ></path>
+                                  </svg>
+                                </span>
+                                <span className="wave-right">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="object-cover w-full h-full"
+                                    viewBox="0 0 487 487"
+                                  >
+                                    <path
+                                      fillOpacity=".1"
+                                      fillRule="nonzero"
+                                      fill="#FFF"
+                                      d="M487 486.7c-66.1-3.6-132.3-7.3-186.3-37s-95.9-85.3-126.2-137.2c-30.4-51.8-49.3-99.9-76.5-151.4C70.9 109.6 35.6 54.8.3 0H487v486.7z"
+                                    ></path>
+                                  </svg>
+                                </span>
+                                <span className="wave-overlay"></span>
+                                <span className="btn-text">
+                                  Explore SniperAI Bot
+                                </span>
+                                <svg
+                                  width="24"
+                                  height="24"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path
+                                    d="M5 12H19M19 12L12 5M19 12L12 19"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                  />
+                                </svg>
+                              </button>
                             </div>
                           </div>
                         )}{" "}
