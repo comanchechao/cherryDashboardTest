4c0ed17eee0039ee445d885212142197b17cf16e
comanchechao <chao.comanche@gmail.com>
2025-08-21 15:18:06 +0330

    Update index.tsx

--- src/pages/rewards/index.tsx
+++ src/pages/rewards/index.tsx
@@ -411,7 +411,7 @@ const Rewards: React.FC = () => {
                     }}
                     className="bg-white/10 cursor-pointer hover:bg-white/20 text-white px-4 py-2 rounded-sm  border border-white/10 winky-sans-font"
                   >
-                    Login With Telegram
+                    Connect Wallet
                   </button>
                 </div>
 
@@ -515,10 +515,10 @@ const Rewards: React.FC = () => {
                                   </a>
                                 </td>
                                 <td className="py-3 pr-4 text-white/80">
-                                  $2,450,320
+                                  $863,449
                                 </td>
                                 <td className="py-3 pr-4 text-white/80">
-                                  $0.0000234
+                                  $0.006765{" "}
                                 </td>
                                 <td className="py-3 pr-4 text-right">
                                   <button className="px-3 py-2 rounded-sm text-sm border bg-amber-400 text-black border-amber-400 hover:bg-amber-300 transition-colors duration-200">
@@ -543,10 +543,10 @@ const Rewards: React.FC = () => {
                                   </a>
                                 </td>
                                 <td className="py-3 pr-4 text-white/80">
-                                  $1,890,450
+                                  $433,633
                                 </td>
                                 <td className="py-3 pr-4 text-white/80">
-                                  $0.0000234
+                                  $0.00688
                                 </td>
                                 <td className="py-3 pr-4 text-right">
                                   <button className="px-3 py-2 rounded-sm text-sm border bg-white/10 text-white border-white/10 hover:bg-white/20 transition-colors duration-200">
@@ -571,10 +571,10 @@ const Rewards: React.FC = () => {
                                   </a>
                                 </td>
                                 <td className="py-3 pr-4 text-white/80">
-                                  $890,120
+                                  $1,477,004
                                 </td>
                                 <td className="py-3 pr-4 text-white/80">
-                                  $0.0000234
+                                  $0.006758
                                 </td>
                                 <td className="py-3 pr-4 text-right">
                                   <button className="px-3 py-2 rounded-sm text-sm border bg-white/10 text-white border-white/10 hover:bg-white/20 transition-colors duration-200">
@@ -599,10 +599,10 @@ const Rewards: React.FC = () => {
                                   </a>
                                 </td>
                                 <td className="py-3 pr-4 text-white/80">
-                                  $890,120
+                                  $4,009,422{" "}
                                 </td>
                                 <td className="py-3 pr-4 text-white/80">
-                                  $0.0000234
+                                  $0.006786
                                 </td>
                                 <td className="py-3 pr-4 text-right">
                                   <button className="px-3 py-2 rounded-sm text-sm border bg-white/10 text-white border-white/10 hover:bg-white/20 transition-colors duration-200">
@@ -919,9 +919,7 @@ const Rewards: React.FC = () => {
                                 height={24}
                                 className="text-white"
                               />
-                              <span className="text-white">
-                                Login with Telegram
-                              </span>
+                              <span className="text-white">Connect Wallet</span>
                             </button>
                           </div>
                         </div>
